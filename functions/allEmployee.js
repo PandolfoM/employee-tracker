41f0c3c6edd565a8e89e2602b06161eeccd2091b
Matthew Pandolfo <matt@pandolfo.com>
2021-10-14 18:31:26 -0400

    departmentcolumn  in View all employees

--- functions/allEmployee.js
+++ functions/allEmployee.js
@@ -2,11 +2,14 @@ const db = require("../db/connection");
 const cTable = require("console.table");
 
 const allEmployee = function () {
-  const sql = `SELECT employee.*, role.title
-                AS title
+  const sql = `SELECT employee.*, 
+                role.title AS title,
+                departments.name AS department
                 FROM employee
-                JOIN role
-                ON employee.role_id = role.id`
+                LEFT JOIN role 
+                ON employee.role_id = role.id
+                LEFT JOIN departments
+                ON role.department_id = departments.id`;
   db.query(sql, (err, result) => {
     const table = cTable.getTable(result);
     console.log(table);
