9377ac4757f80aaf7b1c4bc0b8e85dcc9eb89883
Matthew Pandolfo <matt@pandolfo.com>
2021-10-16 12:31:52 -0400

    updateEmployee with id

--- functions/updateEmployee.js
+++ functions/updateEmployee.js
@@ -1,27 +1,47 @@
 const db = require("../db/connection");
 const inquirer = require("inquirer");
-const allUsers = `SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee`
+const allUsers = `SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee`;
 
-const updateEmployee = function () { 
-  inquirer
-  .prompt([
+const updateEmployee = function () {
+  inquirer.prompt([
     {
-      type: "list",
+      type: "input",
       name: "name",
-      message: "Employee Name:",
-      choices: [
-        
-        ]
+      message: "Employee Id:",
+      validate(answer) {
+        if (!answer) {
+          return "Enter a role title";
+        }
+        return true;
       },
-    ])
-    .then((answer) => {
-      const sql = `INSERT INTO departments (name)
-                VALUES (${JSON.stringify(answer.department)})`;
+    },
+    {
+      type: "input",
+      name: "role",
+      message: "New role id:",
+      validate(answer) {
+        if (!answer) {
+          return "Enter a role id";
+        }
+        return true;
+      },
+    },
+  ])
+  .then((answer) => {
+    const name = JSON.stringify(answer.name)
+    const role = JSON.stringify(answer.role)
+    const sql = `UPDATE employee
+                  SET role_id = ${role}
+                  WHERE id = ${name}`;
 
-      db.query(sql, (err, result) => {
-        console.log(`Added ${answer.department} to Departments`);
-      });
+    db.query(sql, (err, result) => {
+      if (err) {
+        console.log(err.message)
+      } else {
+        console.log(`Updated ${answer.name}'s role`);
+      }
     });
+  });
 };
 
 module.exports = updateEmployee;
